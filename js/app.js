93f9550a1abdd3a410d71990d65c90fb38283b35
Dylan Ellison <dylancellison@gmail.com>
2018-03-10 22:08:55 -0800

    moved model logic outside of ViewModel into cat function

--- js/app.js
+++ js/app.js
@@ -1,26 +1,36 @@
-var ViewModel = function(){
-    this.clickCount = ko.observable(0)
-    this.name = ko.observable('Stormy')
-    this.imgSrc = ko.observable('img/cat1.jpg')
-    this.nickName = ko.observableArray([
-      "cocoPuff", "coolguy", "twoface"
-]);
+var Cat = function(){
 
+      this.clickCount = ko.observable(0)
+      this.name = ko.observable('Stormy')
+      this.imgSrc = ko.observable('img/cat1.jpg')
+      this.nickName = ko.observableArray([
+        "cocoPuff", "coolguy", "twoface"
+  ])
 
-    this.incrementCounter = function() {
-      this.clickCount(this.clickCount() + 1)
+  this.lifeStage = ko.computed(function(){
+
+    if(this.clickCount() < 10){
+      return "baby"
+    }
+    else {
+      return "teenager"
     }
 
-    this.lifeStage = ko.computed(function(){
+  }, this)
 
-      if(this.clickCount() < 10){
-        return "baby"
-      }
-      else {
-        return "teenager"
-      }
 
-    }, this)
+
+}
+
+var ViewModel = function(){
+
+    this.currentCat = ko.observable(new Cat())
+
+    this.incrementCounter = function() {
+      this.currentCat().clickCount(this.currentCat().clickCount() + 1)
+    }
+
+
 
 
 
